2441558c04350ed1d02f2ad526917392f3060ca8
benoît <bra@synapsia.fr>
2025-12-11 17:06:10 -0500

    update doc comments to use backticks for tags in `helper.dart`

--- lib/CORE/test/multi_cases/helper.dart
+++ lib/CORE/test/multi_cases/helper.dart
@@ -8,19 +8,19 @@ export 'test_input.dart';
 /// [function] is the function to test.
 /// [testCases] is the list of test cases.
 /// [caseDescription] is the description of the test.
-/// You can use tags like [input] and [expect] in the description.
+/// You can use tags like `[input]` and `[expect]` in the description.
 /// They will be replaced by the values of the test cases.
 ///
 /// Tag list:
-/// - [input]: the input of the test case
-/// - [n]: if the input is a list,
+/// - `[input]`: the input of the test case
+/// - `[n]`: if the input is a list,
 ///   it will be replaced by the n-th element of the list
 ///   for example, if the input is ['a', 'b', 'c'],
-///   [1] will be replaced by 'a'
-///   [2] will be replaced by 'b'
-///   [3] will be replaced by 'c'
-/// - [expect]: the expected output of the test case
-/// - [expect?value1:value2]: if the expected output is true,
+///   `[1]` will be replaced by ['a']
+///   `[2]` will be replaced by ['b']
+///   `[3]` will be replaced by ['c']
+/// - `[expect]`: the expected output of the test case
+/// - `[expect?value1:value2]`: if the expected output is true,
 ///   it will be replaced by value1, otherwise by value2
 ///
 /// Example:
