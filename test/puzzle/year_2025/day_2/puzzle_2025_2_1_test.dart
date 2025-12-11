f9bbe53b9691aa134bc9528841fd27b9c627a027
benoît <bra@synapsia.fr>
2025-12-11 13:13:26 -0500

    update test files to use `puzzle_tests.dart` and `TestCase` class
    - update imports from `run_tests.dart` to `puzzle_tests.dart`
    - replace `TestData` with `TestCase` in day 2 test
    - access input via index 0 for `isValidID` check

--- test/puzzle/year_2025/day_2/puzzle_2025_2_1_test.dart
+++ test/puzzle/year_2025/day_2/puzzle_2025_2_1_test.dart
@@ -1,4 +1,4 @@
-import 'package:advent_of_code__dart/CORE/test/run_tests.dart';
+import 'package:advent_of_code__dart/CORE/test/puzzle_tests.dart';
 import 'package:advent_of_code__dart/puzzle/year_2025/day_2/puzzle_2025_2_1.dart';
 
 void main() {
@@ -14,15 +14,15 @@ void main() {
 
       group('isValidID()', () {
         final testCases = [
-          TestData('11', expect: false),
-          TestData('99', expect: false),
-          TestData('1010', expect: false),
-          TestData('123123', expect: false),
-          TestData('123124', expect: true),
+          TestCase('11', expect: false),
+          TestCase('99', expect: false),
+          TestCase('1010', expect: false),
+          TestCase('123123', expect: false),
+          TestCase('123124', expect: true),
         ];
         for (final testCase in testCases) {
           test(testCase.toString(), () {
-            expect(isValidID(testCase.input), testCase.expect);
+            expect(isValidID(testCase.input[0]), testCase.expect);
           });
         }
       });
