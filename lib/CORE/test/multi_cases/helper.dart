9398736e7125c64261f5550fb5c5ad4b89ca8e20
benoît <bra@synapsia.fr>
2025-12-11 14:07:13 -0500

    swap arguments `testCases` and `caseDescription` in `testOnCases`

--- lib/CORE/test/multi_cases/helper.dart
+++ lib/CORE/test/multi_cases/helper.dart
@@ -6,10 +6,10 @@ export 'test_input.dart';
 
 /// Test a function on a list of test cases.
 /// [function] is the function to test.
+/// [testCases] is the list of test cases.
 /// [caseDescription] is the description of the test.
 /// You can use tags like [input] and [expect] in the description.
 /// They will be replaced by the values of the test cases.
-/// [testCases] is the list of test cases.
 ///
 /// Tag list:
 /// - [input]: the input of the test case
@@ -19,13 +19,13 @@ export 'test_input.dart';
 /// ```dart
 /// testOnCases(
 ///   parseDirection,
-///   '[input] - direction must be [expect]',
 ///   [
 ///     TestCase('L68', expect: Direction.left),
 ///     TestCase('L642', expect: Direction.left),
 ///     TestCase('R4', expect: Direction.right),
 ///     TestCase('R48', expect: Direction.right),
-///   ]
+///   ],
+///   '[input] - direction must be [expect]',
 /// );
 ///
 
