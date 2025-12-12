8884d19a89ba98fbfd45c665fb5cf18b40f04038
benoît <bra@synapsia.fr>
2025-12-12 09:35:46 -0500

    add boilerplate for puzzle 2025 3 1

---
+++ lib/puzzle/year_2025/day_3/puzzle_2025_3_1.dart
@@ -0,0 +1,15 @@
+import 'package:advent_of_code__dart/CORE/entity/puzzle.dart';
+
+final class Puzzle_2025_3_1 extends Puzzle {
+  Puzzle_2025_3_1(): super(year: 2025, day: 3, part: 1);
+
+  @override
+  // TODO: implement exampleAnswer
+  get exampleAnswer => throw UnimplementedError();
+
+  @override
+  solve(String input, [bool verbose = false]) {
+    // TODO: implement solve
+    throw UnimplementedError();
+  }
+}
---
+++ test/puzzle/year_2025/day_3/2025_3_1_test.dart
@@ -0,0 +1,13 @@
+import 'package:advent_of_code__dart/CORE/test/puzzle_tests.dart';
+import 'package:advent_of_code__dart/puzzle/year_2025/day_3/puzzle_2025_3_1.dart';
+
+void main() {
+  runPuzzleTests(Puzzle_2025_3_1(),
+
+    hints: [],
+
+    tests: (puzzle) {
+
+    },
+  );
+}
